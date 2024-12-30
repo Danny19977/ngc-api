7e920de4d70caa2818f016a91f0925716bc63421
what-markeart <what-markeart@gmail.com>
2024-12-30 14:04:22 +0100

    user

--- models/user.go
+++ models/user.go
@@ -5,13 +5,16 @@ import "gorm.io/gorm"
 type user struct {
 	gorm.Model
 
-	name        string
-	postnom     string
-	prenom      string
-	tranche_age string
-	email       string
-	telephone   string
-	bio         string
-	role        string
-	permission  string
+	name                string
+	postnom             string
+	prenom              string
+	tranche_age         string
+	email               string
+	telephone           string
+	bio                 string
+	role                string
+	permission          string
+	type_compte         string
+	status_compte       string
+	agree_terme_service bool
 }
